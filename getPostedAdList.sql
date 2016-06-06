511bdd412cf298966928420c6bb78500dfb3b5a2
anandvb <anand@vibeosys.com>
2016-06-06 19:56:47 +0530

    Posted ad and saved ads stored procs
    Stored procs for posted and saved ads

--- getPostedAdList.sql
+++ getPostedAdList.sql
@@ -9,51 +9,77 @@ declare maxDistance int;
 declare pageSize int;
 declare lowerPageLimit int;
 declare upperPageLimit int;
-declare initQuery varchar(5000);
-declare midQuery varchar(500);
-declare laterQuery varchar(500);
 
-select cast(ConfigValue as signed) into maxDistance from config_settings where ConfigKey = 'AdSearchDistanceInKM';
-select cast(ConfigValue as signed) into pageSize from config_settings where ConfigKey = 'ClassifiedAdPageSize';
+SELECT 
+    CAST(ConfigValue AS SIGNED)
+INTO maxDistance FROM
+    config_settings
+WHERE
+    ConfigKey = 'AdSearchDistanceInKM';
+SELECT 
+    CAST(ConfigValue AS SIGNED)
+INTO pageSize FROM
+    config_settings
+WHERE
+    ConfigKey = 'ClassifiedAdPageSize';
 -- select maxDistance;
 IF pageNumber = 1 THEN
 	set lowerPageLimit = 0, upperPageLimit = pageSize;
 else
-	set lowerPageLimit = (pageNumber - 1) * pageSize, upperPageLimit = pageNumber * pageSize;
+	set lowerPageLimit = (pageNumber - 1) * pageSize;
+    set upperPageLimit = pageSize;
 END IF;    
 
-if sortChoice = "PostedDate" and sortOption = "DESC" then
-select AdId,AdTitle,Description,DisplayImgUrl,PostedDate,Price,( 6371 * acos( cos( radians(requestedLat) ) * cos( radians( Latitude ) ) * cos( radians( Longitude ) - radians(requestedLong) ) + sin( radians(requestedLat) ) * sin( radians( Latitude ) ) ) ) AS Distance FROM posted_ad HAVING Distance <  maxDistance
+if sortOption = "PostedDate" and sortChoice = "DESC" then
+select AdId,AdTitle,Description,DisplayImgUrl,PostedDate,Price,( 6371 * acos( cos( radians(requestedLat) ) * cos( radians( Latitude ) ) * cos( radians( Longitude ) - radians(requestedLong) ) + sin( radians(requestedLat) ) * sin( radians( Latitude ) ) ) ) AS Distance 
+FROM posted_ad 
+where StatusId = 0
+HAVING Distance <  maxDistance
 Order by PostedDate DESC
 LIMIT lowerPageLimit , upperPageLimit;
 
-elseif sortChoice = "PostedDate" and sortOption = "ASC" then
+elseif sortOption = "PostedDate" and sortChoice = "ASC" then
 
-select AdId,AdTitle,Description,DisplayImgUrl,PostedDate,Price,( 6371 * acos( cos( radians(requestedLat) ) * cos( radians( Latitude ) ) * cos( radians( Longitude ) - radians(requestedLong) ) + sin( radians(requestedLat) ) * sin( radians( Latitude ) ) ) ) AS Distance FROM posted_ad HAVING Distance <  maxDistance
+select AdId,AdTitle,Description,DisplayImgUrl,PostedDate,Price,( 6371 * acos( cos( radians(requestedLat) ) * cos( radians( Latitude ) ) * cos( radians( Longitude ) - radians(requestedLong) ) + sin( radians(requestedLat) ) * sin( radians( Latitude ) ) ) ) AS Distance 
+FROM posted_ad 
+where StatusId = 0
+HAVING Distance <  maxDistance
 Order by PostedDate ASC
 LIMIT lowerPageLimit , upperPageLimit;
 
-elseif sortChoice = "Price" and sortOption = "DESC" then
+elseif sortOption = "Price" and sortChoice = "DESC" then
 
-select AdId,AdTitle,Description,DisplayImgUrl,PostedDate,Price,( 6371 * acos( cos( radians(requestedLat) ) * cos( radians( Latitude ) ) * cos( radians( Longitude ) - radians(requestedLong) ) + sin( radians(requestedLat) ) * sin( radians( Latitude ) ) ) ) AS Distance FROM posted_ad HAVING Distance <  maxDistance
+select AdId,AdTitle,Description,DisplayImgUrl,PostedDate,Price,( 6371 * acos( cos( radians(requestedLat) ) * cos( radians( Latitude ) ) * cos( radians( Longitude ) - radians(requestedLong) ) + sin( radians(requestedLat) ) * sin( radians( Latitude ) ) ) ) AS Distance 
+FROM posted_ad 
+where StatusId = 0
+HAVING Distance <  maxDistance
 Order by Price DESC
 LIMIT lowerPageLimit , upperPageLimit;
 
-elseif sortChoice = "Price" and sortOption = "ASC" then
+elseif sortOption = "Price" and sortChoice = "ASC" then
 
-select AdId,AdTitle,Description,DisplayImgUrl,PostedDate,Price,( 6371 * acos( cos( radians(requestedLat) ) * cos( radians( Latitude ) ) * cos( radians( Longitude ) - radians(requestedLong) ) + sin( radians(requestedLat) ) * sin( radians( Latitude ) ) ) ) AS Distance FROM posted_ad HAVING Distance <  maxDistance
+select AdId,AdTitle,Description,DisplayImgUrl,PostedDate,Price,( 6371 * acos( cos( radians(requestedLat) ) * cos( radians( Latitude ) ) * cos( radians( Longitude ) - radians(requestedLong) ) + sin( radians(requestedLat) ) * sin( radians( Latitude ) ) ) ) AS Distance
+FROM posted_ad 
+where StatusId = 0
+HAVING Distance <  maxDistance
 Order by Price ASC
 LIMIT lowerPageLimit , upperPageLimit;
 
-elseif sortChoice = "Distance" and sortOption = "DESC" then
+elseif sortOption = "Distance" and sortChoice = "DESC" then
 
-select AdId,AdTitle,Description,DisplayImgUrl,PostedDate,Price,( 6371 * acos( cos( radians(requestedLat) ) * cos( radians( Latitude ) ) * cos( radians( Longitude ) - radians(requestedLong) ) + sin( radians(requestedLat) ) * sin( radians( Latitude ) ) ) ) AS Distance FROM posted_ad HAVING Distance <  maxDistance
+select AdId,AdTitle,Description,DisplayImgUrl,PostedDate,Price,( 6371 * acos( cos( radians(requestedLat) ) * cos( radians( Latitude ) ) * cos( radians( Longitude ) - radians(requestedLong) ) + sin( radians(requestedLat) ) * sin( radians( Latitude ) ) ) ) AS Distance
+FROM posted_ad 
+where StatusId = 0
+HAVING Distance <  maxDistance
 Order by Distance DESC
 LIMIT lowerPageLimit , upperPageLimit;
 
-elseif sortChoice = "Distance" and sortOption = "ASC" then
+elseif sortOption = "Distance" and sortChoice = "ASC" then
 
-select AdId,AdTitle,Description,DisplayImgUrl,PostedDate,Price,( 6371 * acos( cos( radians(requestedLat) ) * cos( radians( Latitude ) ) * cos( radians( Longitude ) - radians(requestedLong) ) + sin( radians(requestedLat) ) * sin( radians( Latitude ) ) ) ) AS Distance FROM posted_ad HAVING Distance <  maxDistance
+select AdId,AdTitle,Description,DisplayImgUrl,PostedDate,Price,( 6371 * acos( cos( radians(requestedLat) ) * cos( radians( Latitude ) ) * cos( radians( Longitude ) - radians(requestedLong) ) + sin( radians(requestedLat) ) * sin( radians( Latitude ) ) ) ) AS Distance 
+FROM posted_ad 
+where StatusId = 0
+HAVING Distance <  maxDistance
 Order by Distance ASC
 LIMIT lowerPageLimit , upperPageLimit;
 
